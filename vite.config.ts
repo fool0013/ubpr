12072eda9c255ab02a9caf3f82bf318da940c115
Fool <78437644+fool0013@users.noreply.github.com>
2026-02-23 04:22:54 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2024-2025 Pagefault Games
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-
-import { defineConfig, loadEnv, type UserConfig, type UserConfigFnPromise } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-/**
- * Default config object used for both Vitest and local dev runs.
- */
-export const sharedConfig: UserConfigFnPromise = async ({ mode }) =>
-  ({
-    // Avoid importing any plugins when merging reports (as they will not exist at runtime)
-    plugins: process.env.MERGE_REPORTS
-      ? []
-      : [
-          tsconfigPaths(),
-          (await import("./src/plugins/vite/vite-minify-json-plugin")).minifyJsonPlugin(
-            ["images", "battle-anims"],
-            true,
-          ),
-          (await import("./src/plugins/vite/namespaces-i18n-plugin")).LocaleNamespace(),
-        ],
-    clearScreen: false,
-    appType: "mpa",
-    build: {
-      chunkSizeWarningLimit: 10000,
-      minify: "esbuild",
-      sourcemap: mode !== "production",
-      rollupOptions: {
-        onwarn(warning, defaultHandler) {
-          // Suppress "Module level directives cause errors when bundled" warnings
-          if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-            return;
-          }
-          defaultHandler(warning);
-        },
-      },
-    },
-    esbuild: {
-      pure: mode === "production" ? ["console.log"] : [],
-      keepNames: true,
-    },
-  }) satisfies UserConfig;
-
-// biome-ignore lint/style/noDefaultExport: required for Vite
 export default defineConfig(async config => {
   const { mode, command } = config;
   const envPort = Number(loadEnv(mode, process.cwd()).VITE_PORT);
@@ -53,7 +5,7 @@ export default defineConfig(async config => {
   return {
     ...(await sharedConfig(config)),
     base: "/ubpr/",
-    publicDir: command === "serve" ? "assets" : false,
+    publicDir: "public", // Change this to strictly "public"
     server: {
       port: Number.isNaN(envPort) ? 8000 : envPort,
     },
